f8c34003fb107088eea11cb309b15f2f2fc99516
Joeph-Anthony-King <Joseph.King.CA@gmail.com>
2019-05-10 12:21:10 -0700

    Initiate repository

---
+++ SudokuApp.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SudokuApp.ConsoleApp {
+
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("\nHello World from the SudokuApp Console App!");
+            Console.ReadLine();
+        }
+    }
+}
