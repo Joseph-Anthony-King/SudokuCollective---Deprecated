edba9313f915a1be682c4e8da9d9c5e797a23615
Joeph-Anthony-King <Joseph.King.CA@gmail.com>
2019-05-20 13:31:37 -0700

    Add time limit to solution method

--- SudokuApp.ConsoleApp/Routines/SolveSolutions.cs
+++ SudokuApp.ConsoleApp/Routines/SolveSolutions.cs
@@ -12,14 +12,15 @@ namespace SudokuApp.ConsoleApp.Routines {
 
             Console.WriteLine("\nPlease enter the sudoku puzzle you wish to solve.");
             Console.WriteLine("You will be entering the nine values for each row.");
-            Console.WriteLine("Just enter the values with no spaces, For unknown");
+            Console.WriteLine("Just enter the values with no spaces, for unknown");
             Console.WriteLine("values enter 0.  Once you're done the solver will");
-            Console.WriteLine("produce your answer.\n");
+            Console.WriteLine("produce an answer if possible from the provided.");
+            Console.WriteLine("values.  If it cannot produce an answer the solver");
+            Console.WriteLine("will let you know.\n");
             Console.WriteLine("Press enter to continue!");
 
             Console.ReadLine();
 
-            Stopwatch stopwatch = new Stopwatch();
             var continueLoop = true;
 
             do {
@@ -63,39 +64,47 @@ namespace SudokuApp.ConsoleApp.Routines {
                 response.Append(Console.ReadLine());
                 Console.WriteLine();
 
-                stopwatch.Start();
                 var matrix = new SudokuSolver(response.ToString());
                 matrix.Solve();
-                matrix.SetDifficulty(Difficulty.TEST);
-                stopwatch.Stop();
 
-                foreach (var row in matrix.Rows) {
+                if (matrix.IsValid()) {
+                    matrix.SetDifficulty(Difficulty.TEST);
 
-                    foreach (var cell in row) {
+                    foreach (var row in matrix.Rows) {
 
-                        Console.Write(string.Format("{0} ", cell));
+                        foreach (var cell in row) {
+
+                            Console.Write(string.Format("{0} ", cell));
+                        }
+
+                        Console.WriteLine();
                     }
 
-                    Console.WriteLine();
+                    // Format and display the TimeSpan value.
+                    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                        matrix.stopwatch.Elapsed.Hours,
+                        matrix.stopwatch.Elapsed.Minutes,
+                        matrix.stopwatch.Elapsed.Seconds,
+                        matrix.stopwatch.Elapsed.Milliseconds / 10);
+
+                    Console.Write("\nTime to generate solution: " + elapsedTime + "\n\n");
+
+                } else {
+
+                    Console.WriteLine("Need more values in order to deduce a possible solution.\n");
                 }
-
-                // Format and display the TimeSpan value.
-                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                    stopwatch.Elapsed.Hours,
-                    stopwatch.Elapsed.Minutes,
-                    stopwatch.Elapsed.Seconds,
-                    stopwatch.Elapsed.Milliseconds / 10);
-
-                Console.Write("\nTime to generate solution: " + elapsedTime + "\n\n");
 
                 Console.Write("Would you like to solve another solution (yes/no): ");
 
                 var result = Console.ReadLine();
-                Console.WriteLine();
 
                 if (result.ToLower().Equals("no") || result.ToLower().Equals("n")) {
 
                     continueLoop = false;
+
+                } else {
+
+                    Console.WriteLine();
                 }
 
             } while (continueLoop);
