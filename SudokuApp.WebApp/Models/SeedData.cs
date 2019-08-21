784480a898cb85635b861dad871d7917b1f4123a
Joeph-Anthony-King <Joseph.King.CA@gmail.com>
2019-08-21 16:22:02 -0700

    Add admin user seed data

--- SudokuApp.WebApp/Models/SeedData.cs
+++ SudokuApp.WebApp/Models/SeedData.cs
@@ -75,6 +75,44 @@ namespace SudokuApp.WebApp.Models {
 
                     context.SaveChanges();
                 }
+
+                if (!context.Users.Any()) {
+
+                    context.Users.AddRange(
+
+                        new User {
+
+                            FirstName = "Admin",
+                            LastName = "User",
+                            NickName = "Administrator",
+                            DateCreated = DateTime.Now,
+                            Email = "Joseph.King.CA@gmail.com",
+                            Password = "P@ssw0rd"
+                        }
+                    );
+
+                    context.SaveChanges();
+                }
+
+                if (!context.UsersPermissions.Any()) {
+
+                    context.UsersPermissions.AddRange(
+
+                        new UserPermission {
+
+                            UserId = 1,
+                            PermissionId = 1
+                        },
+
+                        new UserPermission {
+
+                            UserId = 1,
+                            PermissionId = 2
+                        }
+                    );
+
+                    context.SaveChanges();
+                }
             }
         }
     }
