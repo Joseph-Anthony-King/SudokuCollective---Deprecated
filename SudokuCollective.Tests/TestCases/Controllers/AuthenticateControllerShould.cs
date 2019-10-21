a00c229625b9cdc039535d41ff051c6e3a4dc17d
Joeph-Anthony-King <Joseph.King.CA@gmail.com>
2019-10-21 12:54:45 -0700

    Add tests for the hello world and authenticate controllers

---
+++ SudokuCollective.Tests/TestCases/Controllers/AuthenticateControllerShould.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SudokuCollective.WebApi.Controllers;
+using SudokuCollective.WebApi.Models.TokenModels;
+using SudokuCollective.WebApi.Services.Interfaces;
+
+namespace SudokuCollective.Tests.TestCases.Controllers {
+
+    public class AuthenticateControllerShould {
+
+        private AuthenticateController sut;
+        private Mock<IAuthenticateService> mockAuthenticateService;
+        private Mock<IUserManagementService> mockUserManagementService;
+        private TokenRequest tokenRequest;
+        private string userName;
+        private string password;
+
+        [SetUp]
+        public void Setup() {
+
+            userName = "TestSuperUser";
+            password = "password1";
+
+            mockAuthenticateService = new Mock<IAuthenticateService>();
+
+            tokenRequest = new TokenRequest() {
+
+                UserName = userName,
+                Password = password
+            };
+
+            var token = string.Empty;
+
+            mockAuthenticateService.Setup(authService => authService.IsAuthenticated(tokenRequest, out token)).Returns(true);
+
+            mockUserManagementService = new Mock<IUserManagementService>();
+            mockUserManagementService.Setup(service => service.IsValidUser(userName, password)).Returns(Task.FromResult(true));
+
+            sut = new AuthenticateController(mockAuthenticateService.Object);
+        }
+
+        [Test]
+        [Category("Services")]
+        public void AuthenticateUsers() {
+
+            // Arrange
+
+            // Act
+            var result = sut.RequestToken(tokenRequest);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<OkObjectResult>());
+        }
+    }
+}
---
+++ SudokuCollective.Tests/TestCases/Controllers/HelloWorldControllerShould.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using SudokuCollective.WebApi.Controllers;
+
+namespace SudokuCollective.Tests.TestCases.Controllers {
+
+    public class HelloWorldControllerShould {
+
+        [Test]
+        [Category("Controllers")]
+        public void ReturnAMessage() {
+
+            // Arrange
+            var sut = new HelloWorldController();
+
+            // Act
+            var result = sut.Get();
+            var message = ((OkObjectResult)result).Value;
+
+            // Assert
+            Assert.That(result, Is.TypeOf<OkObjectResult>());
+            Assert.That(message, Is.EqualTo("Hello World from Sudoku Collective!"));
+        }
+    }
+}
