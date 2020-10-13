9649f20a8bc278d6665a7b0483a7f4cb34b2060a
Joeph-Anthony-King <Joseph.King.CA@gmail.com>
2020-10-12 17:15:53 -0700

    Update mutation types

--- SudokuCollective.WebApi/client/src/store/mutation-types.js
+++ SudokuCollective.WebApi/client/src/store/mutation-types.js
@@ -1,2 +1,2 @@
-export const CONFIRM_BASE_URL = "confirm";
-export const UPDATE_API_MESSAGE = "update"
+export const CONFIRM_BASE_URL = "confirmBaseUrl";
+export const UPDATE_API_MESSAGE = "updateAPIMessage"
