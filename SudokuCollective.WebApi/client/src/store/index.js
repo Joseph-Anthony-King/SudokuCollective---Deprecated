076db4aff5b9b2742f7a66bed39bd1edce0e98ef
Joeph-Anthony-King <Joseph.King.CA@gmail.com>
2020-10-12 21:48:14 -0700

    Correct issue in vuex store index.js

--- SudokuCollective.WebApi/client/src/store/index.js
+++ SudokuCollective.WebApi/client/src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-import { appConfig } from "./modules/appConfig/appConfig"
+import appConfig from "./modules/appConfig/appConfig"
 
 Vue.use(Vuex);
 
