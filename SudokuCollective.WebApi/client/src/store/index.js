f1621410e6b7f0fb7b3f584700d821e8fe7e914e
Joeph-Anthony-King <Joseph.King.CA@gmail.com>
2020-10-12 17:54:31 -0700

    Add vuex modules

--- SudokuCollective.WebApi/client/src/store/index.js
+++ SudokuCollective.WebApi/client/src/store/index.js
@@ -1,42 +1,9 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-import { baseURLConfirmationService } from "../services/baseURLConfirmation.service";
-import { CONFIRM_BASE_URL, UPDATE_API_MESSAGE } from "./mutation-types";
+import { appConfig } from "./modules/appConfig/appConfig"
 
 Vue.use(Vuex);
-
-const appConfig = {
-
-    namsespaced: false,
-
-    state: () => ({
-        baseURL: "",
-        apiMessage: "Sudoku Collective Admin Vue"
-    }),
-
-    mutations: {
-        [CONFIRM_BASE_URL](state, confirmedURL) {
-            state.baseURL = confirmedURL;
-        },
-        [UPDATE_API_MESSAGE](state, updatedAPIMessage) {
-            state.apiMessage = updatedAPIMessage;
-        }
-    },
-
-    actions: {
-        async confirmBaseURL({ commit }, baseURL) {
-            const response = await baseURLConfirmationService.confirm(baseURL);
-            commit(CONFIRM_BASE_URL, response.url);
-            commit(UPDATE_API_MESSAGE, response.message)
-        }
-    },
-
-    getters: {
-        getBaseURL: state => { return state.baseURL },
-        getAPIMessage: state => { return state.apiMessage }
-    }
-}
 
 export default new Vuex.Store({
     state: {},
