0782cf8ac942ccddefcb17fa45e223718d265499
Joeph-Anthony-King <Joseph.King.CA@gmail.com>
2021-02-19 15:08:36 -0800

    Update user profile form styling

--- SudokuCollective.Api/Client/src/components/pages/UserProfileForm.vue
+++ SudokuCollective.Api/Client/src/components/pages/UserProfileForm.vue
@@ -1,116 +1,121 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12">
-        <h1 style="text-align: center" v-if="user.isActive">
-          Your Account is Active
-        </h1>
-        <h1
-          style="text-align: center"
-          v-if="!user.isActive"
-          class="account-status-warning"
-        >
-          Your Account is Deativated, Please Contact the Administrator
-        </h1>
-      </v-col>
-    </v-row>
-    <v-row style="min-height: 25px"></v-row>
-    <v-row>
-      <v-col cols="6">
-        <v-row>
-          <label class="label">User Name:</label>
-          <span class="label-spacer"></span>
-          <p class="userInfo">{{ user.userName }}</p>
-        </v-row>
-        <v-row>
-          <label class="label">First Name:</label>
-          <span class="label-spacer"></span>
-          <p class="userInfo">{{ user.firstName }}</p>
-        </v-row>
-        <v-row>
-          <label class="label">Last Name:</label>
-          <span class="label-spacer"></span>
-          <p class="userInfo">{{ user.lastName }}</p>
-        </v-row>
-        <v-row>
-          <label class="label">Nickname:</label>
-          <span class="label-spacer"></span>
-          <p class="userInfo">{{ user.nickName }}</p>
-        </v-row>
-        <v-row>
-          <label class="label">Admin Privileges:</label>
-          <span class="label-spacer"></span>
-          <v-icon v-if="user.isAdmin" color="green">mdi-check</v-icon>
-          <v-icon v-if="!user.isAdmin" color="red">mdi-close</v-icon>
-        </v-row>
-      </v-col>
-      <v-col cols="6">
-        <v-row>
-          <label class="label">Email:</label>
-          <span class="label-spacer"></span>
-          <p class="userInfo">{{ user.email }}</p>
-        </v-row>
-        <v-row>
-          <label class="label">Email Confirmed:</label>
-          <span class="label-spacer"></span>
-          <v-icon v-if="user.emailConfirmed" color="green">mdi-check</v-icon>
-          <v-icon v-if="!user.emailConfirmed" color="red">mdi-close</v-icon>
-        </v-row>
-        <v-row style="min-height: 25px"></v-row>
-        <v-row>
-          <label class="label">Date Created:</label>
-          <span class="label-spacer"></span>
-          <p class="userInfo">
-            {{ new Date(user.dateCreated).toLocaleString() }}
-          </p>
-        </v-row>
-        <v-row v-if="user.dateUpdated !== '0001-01-01T00:00:00Z'">
-          <label class="label">Date Updated:</label>
-          <span class="label-spacer"></span>
-          <p class="userInfo">
-            {{ new Date(user.dateUpdated).toLocaleString() }}
-          </p>
-        </v-row>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col col="12">
-        <h2
-          class="account-status"
-          v-if="
-            !user.receivedRequestToUpdateEmail &&
-            !user.receivedRequestToUpdatePassword &&
-            user.emailConfirmed
-          "
-        >
-          No Outstanding Requests for this Account
-        </h2>
-        <h2 class="account-status-warning" v-if="!user.emailConfirmed">
-          Please Confirm Your Email
-        </h2>
-        <h2
-          class="account-status-warning"
-          v-if="user.receivedRequestToUpdateEmail"
-        >
-          Received Request to Update Email
-        </h2>
-        <h2
-          class="account-status-warning"
-          v-if="user.receivedRequestToUpdatePassword"
-        >
-          Received Request to Update Password
-        </h2>
-      </v-col>
-    </v-row>
-    <div style="padding: 50px 0px 0px 0px"></div>
+  <v-container fluid>
+    <v-card elevation="6">
+      <v-card-title v-if="user.isActive" class="justify-center"
+        >Your Account is Active</v-card-title
+      >
+      <v-card-title v-if="!user.isActive" class="justify-center warning"
+        >Your Account is Deativated, Please Contact the
+        Administrator</v-card-title
+      >
+    </v-card>
     <hr />
-    <v-row>
-      <v-col col="12">
-        <p class="available-actions">Available Actions</p>
-      </v-col>
-    </v-row>
-    <v-row style="text-align: center">
-      <v-col cols="12">
+    <v-card elevation="6" class="mx-auto user-card">
+      <v-layout>
+        <v-card-text>
+          <v-row>
+            <v-col col="6">
+              <v-row>
+                <label class="label">User Name:</label>
+                <span class="label-spacer"></span>
+                <p class="userInfo">{{ user.userName }}</p>
+              </v-row>
+              <v-row>
+                <label class="label">First Name:</label>
+                <span class="label-spacer"></span>
+                <p class="userInfo">{{ user.firstName }}</p>
+              </v-row>
+              <v-row>
+                <label class="label">Last Name:</label>
+                <span class="label-spacer"></span>
+                <p class="userInfo">{{ user.lastName }}</p>
+              </v-row>
+              <v-row>
+                <label class="label">Nickname:</label>
+                <span class="label-spacer"></span>
+                <p class="userInfo">{{ user.nickName }}</p>
+              </v-row>
+              <v-row>
+                <label class="label">Admin Privileges:</label>
+                <span class="label-spacer"></span>
+                <v-icon v-if="user.isAdmin" color="green">mdi-check</v-icon>
+                <v-icon v-if="!user.isAdmin" color="red">mdi-close</v-icon>
+              </v-row>
+              <v-row class="row-spacer"></v-row>
+              <v-row v-if="user.isSuperUser">
+                <label class="label">Super User Privileges:</label>
+                <span class="label-spacer"></span>
+                <v-icon v-if="user.isSuperUser" color="green">mdi-check</v-icon>
+                <v-icon v-if="!user.isSuperUser" color="red">mdi-close</v-icon>
+              </v-row>
+            </v-col>
+            <v-col col="6">
+              <v-row>
+                <label class="label">Email:</label>
+                <span class="label-spacer"></span>
+                <p class="userInfo">{{ user.email }}</p>
+              </v-row>
+              <v-row>
+                <label class="label">Email Confirmed:</label>
+                <span class="label-spacer"></span>
+                <v-icon v-if="user.emailConfirmed" color="green"
+                  >mdi-check</v-icon
+                >
+                <v-icon v-if="!user.emailConfirmed" color="red"
+                  >mdi-close</v-icon
+                >
+              </v-row>
+              <v-row class="row-spacer"></v-row>
+              <v-row>
+                <label class="label">Date Created:</label>
+                <span class="label-spacer"></span>
+                <p class="userInfo">
+                  {{ new Date(user.dateCreated).toLocaleString() }}
+                </p>
+              </v-row>
+              <v-row v-if="user.dateUpdated !== '0001-01-01T00:00:00Z'">
+                <label class="label">Date Updated:</label>
+                <span class="label-spacer"></span>
+                <p class="userInfo">
+                  {{ new Date(user.dateUpdated).toLocaleString() }}
+                </p>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-layout>
+    </v-card>
+    <div class="card-spacer"></div>
+    <v-card elevation="6">
+      <v-card-title
+        v-if="
+          !user.receivedRequestToUpdateEmail &&
+          !user.receivedRequestToUpdatePassword &&
+          user.emailConfirmed
+        "
+        class="justify-center"
+        >No Outstanding Requests for this Account</v-card-title
+      >
+      <v-card-title v-if="!user.emailConfirmed" class="justify-center warning"
+        >Please Confirm Your Email</v-card-title
+      >
+      <v-card-title
+        v-if="user.receivedRequestToUpdateEmail"
+        class="justify-center warning"
+        >Received Request to Update Email</v-card-title
+      >
+      <v-card-title
+        v-if="user.receivedRequestToUpdatePassword"
+        class="justify-center warning"
+        >Received Request to Update Password</v-card-title
+      >
+    </v-card>
+    <hr />
+    <v-card elevation="6">
+      <v-card-title v-if="user.isActive" class="justify-center"
+        >Available Actions</v-card-title
+      >
+      <v-card-actions class="justify-center">
         <v-btn color="blue darken-1" text @click="refreshProfile">
           Refresh Profile
         </v-btn>
@@ -121,7 +126,11 @@
           color="blue darken-1"
           text
           @click="cancelEmailConfirmation"
-          v-if="user.receivedRequestToUpdateEmail || (!user.emailConfirmed && user.dateUpdated !== '0001-01-01T00:00:00Z')"
+          v-if="
+            user.receivedRequestToUpdateEmail ||
+            (!user.emailConfirmed &&
+              user.dateUpdated !== '0001-01-01T00:00:00Z')
+          "
         >
           Cancel Email Confirmation
         </v-btn>
@@ -145,12 +154,17 @@
           color="blue darken-1"
           text
           @click="cancelAllEmailRequests"
-          v-if="(user.receivedRequestToUpdateEmail || (!user.emailConfirmed && user.dateUpdated !== '0001-01-01T00:00:00Z')) && user.receivedRequestToUpdatePassword"
+          v-if="
+            (user.receivedRequestToUpdateEmail ||
+              (!user.emailConfirmed &&
+                user.dateUpdated !== '0001-01-01T00:00:00Z')) &&
+            user.receivedRequestToUpdatePassword
+          "
         >
           Cancel All Email Requests
         </v-btn>
-      </v-col>
-    </v-row>
+      </v-card-actions>
+    </v-card>
 
     <v-dialog v-model="editingProfile" persistent max-width="600px">
       <EditProfileForm
@@ -164,23 +178,24 @@
 <style scoped>
 .label {
   font-weight: bold;
-  font-size: x-large;
-}
-.userInfo {
-  font-size: x-large;
+  font-size: large;
 }
 .label-spacer {
   min-width: 10px;
 }
-.account-status {
-  text-align: center;
+.row-spacer {
+  min-height: 25px;
 }
-.available-actions {
-  padding: 25px 0 0 0;
-  text-align: center;
+.user-card {
+  padding: 10px 20px 20px 20px;
 }
-.account-status-warning {
-  text-align: center;
+.userInfo {
+  font-size: large;
+}
+.card-spacer {
+  min-height: 30px;
+}
+.warning {
   color: red;
 }
 </style>
@@ -255,7 +270,9 @@ export default {
             toastObject.goAway(0);
 
             try {
-              const response = await userService.putCancelPasswordReset(new PageListModel());
+              const response = await userService.putCancelPasswordReset(
+                new PageListModel()
+              );
 
               if (response.status === 200) {
                 store.dispatch("userModule/updateUser", response.data.user);
@@ -307,7 +324,9 @@ export default {
             toastObject.goAway(0);
 
             try {
-              const response = await userService.putCancelEmailConfirmation(new PageListModel());
+              const response = await userService.putCancelEmailConfirmation(
+                new PageListModel()
+              );
 
               if (response.status === 200) {
                 store.dispatch("userModule/updateUser", response.data.user);
@@ -359,7 +378,9 @@ export default {
             toastObject.goAway(0);
 
             try {
-              const response = await userService.putCancelAllEmailRequests(new PageListModel());
+              const response = await userService.putCancelAllEmailRequests(
+                new PageListModel()
+              );
 
               if (response.status === 200) {
                 store.dispatch("userModule/updateUser", response.data.user);
