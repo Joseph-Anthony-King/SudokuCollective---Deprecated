b39e864b23510e5263167ea572680d0e8df193f9
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2021-02-25 19:26:05 -0800

    Update home page

--- SudokuCollective.Api/Client/src/components/pages/HomeForm.vue
+++ SudokuCollective.Api/Client/src/components/pages/HomeForm.vue
@@ -1,34 +1,30 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img src="/logo.png" class="my-3" contain height="200" />
-      </v-col>
-
-      <v-col class="ml-16 pl-16 mr-16 pr-16">
-        <h1 class="display-2 font-weight-bold">
-          Welcome to {{ adminApp.name }}
-        </h1>
-
-        <p class="subheading font-weight-regular">
+  <v-container fluid>
+    <v-card elevation="6" class="mx-auto">
+      <h1 class="text-center text-padding">Welcome to {{ adminApp.name }}</h1>
+      <v-row class="text-center">
+        <v-col cols="12">
+          <v-img src="/logo.png" class="my-3" contain height="200" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <h3 class="text-center text-padding">
           Sudoku Collective is a project that serves as a ready made Web API
           that you can use to learn client side technologies. The API is
           documented so you can create your own client app which can fully
           integrate with the API. My particular implementation will include
           console and Vue apps.
-        </p>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
-    </v-row>
+        </h3>
+      </v-row>
+    </v-card>
   </v-container>
 </template>
+
+<style scoped>
+.text-padding {
+  padding: 50px 70px 50px 70px;
+}
+</style>
 
 <script>
 import App from "@/models/app";
@@ -36,7 +32,7 @@ import App from "@/models/app";
 export default {
   name: "HomeForm",
   data: () => ({
-    adminApp: {}
+    adminApp: {},
   }),
   created() {
     this.$data.adminApp = new App();
