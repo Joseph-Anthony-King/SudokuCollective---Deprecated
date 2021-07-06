0df6f1309e78c59b3e15845ea4e6390222ab87b4
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2021-07-05 23:38:11 -0700

    Add JWT Token to swager ui

--- SudokuCollective.Api/Startup.cs
+++ SudokuCollective.Api/Startup.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -20,7 +21,6 @@ using SudokuCollective.Data.Services;
 using SudokuCollective.Data.Repositories;
 using SudokuCollective.Api.Models;
 using SudokuCollective.Data.Models.DataModels;
-using System;
 
 namespace SudokuCollective.Api
 {
@@ -56,6 +56,35 @@ namespace SudokuCollective.Api
             services.Configure<TokenManagement>(Configuration.GetSection("TokenManagement"));
             var token = Configuration.GetSection("tokenManagement").Get<TokenManagement>();
             var secret = Encoding.ASCII.GetBytes(token.Secret);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "Sudoku Collective API", Version = "v1" });
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...\""
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
 
             services.AddAuthentication(x =>
             {
@@ -100,12 +129,6 @@ namespace SudokuCollective.Api
             services.AddScoped<IRolesService, RolesService>();
             services.AddScoped<ISolutionsService, SolutionsService>();
             services.AddScoped<IEmailService, EmailService>();
-
-            // Register the Swagger generator, defining 1 or more Swagger documents
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sudoku Collective API", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
